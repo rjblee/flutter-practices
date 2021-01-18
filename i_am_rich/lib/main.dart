04ff21d39f9a7f2f2d9cd1345dd9f8e0e1a941f4
Ralph Lee <ralph.lee@live.ca>
2021-01-19 01:31:16 +0900

    Add appbar and image

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello World"),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text("I Am Rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://i.pinimg.com/originals/20/60/2d/20602d43cc993811e5a6bd1886af4f33.png'),
+          ),
+        ),
       ),
     ),
   );
